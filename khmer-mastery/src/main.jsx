a9878299aa41acb87b703a03dc596e36749fce62
FIJY <fijy@mail.ru>
2026-01-11 10:17:50 +0300

    Fix: Disable PWA cache to fix mobile white screen

--- khmer-mastery/src/main.jsx
+++ khmer-mastery/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- ВОТ ЭТА СТРОЧКА ВКЛЮЧАЕТ СТИЛИ
+import './index.css'
+
+// Мы специально НЕ импортируем никаких sw или pwa, чтобы сбросить кэш
+// import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
