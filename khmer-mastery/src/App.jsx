a3ff254f63762f1d50c2d2f5dcf3716730c1dda5
FIJY <fijy@mail.ru>
2026-01-11 10:30:52 +0300

    Debug: Nuclear option - simple red screen test

--- khmer-mastery/src/App.jsx
+++ khmer-mastery/src/App.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import CourseMap from './pages/CourseMap';
-import LessonPlayer from './pages/LessonPlayer';
-import Welcome from './pages/Welcome';
 
 export default function App() {
   return (
-    <Router>
-      <div className="bg-black min-h-screen text-white">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/map" element={<CourseMap />} />
-          <Route path="/lesson/:id" element={<LessonPlayer />} />
-        </Routes>
-      </div>
-    </Router>
+    <div style={{ height: '100vh', background: 'red', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '24px', fontWeight: 'bold' }}>
+      ТЕСТ: Я ЖИВОЙ!
+      <br />
+      ВЕРСИЯ 3.0
+    </div>
   );
 }
