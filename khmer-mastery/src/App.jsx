c12680376ffc59bfccf01c20f0a7a7da60df2a6e
FIJY <fijy@mail.ru>
2026-01-11 10:45:22 +0300

    Fix: Restore working App.jsx after cache test

--- khmer-mastery/src/App.jsx
+++ khmer-mastery/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import CourseMap from './pages/CourseMap';
+import LessonPlayer from './pages/LessonPlayer';
+import Welcome from './pages/Welcome';
 
 export default function App() {
   return (
-    <div style={{ height: '100vh', background: 'red', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '24px', fontWeight: 'bold' }}>
-      ТЕСТ: Я ЖИВОЙ!
-      <br />
-      ВЕРСИЯ 3.0
-    </div>
+    <Router>
+      <div className="bg-gray-900 min-h-screen text-white">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/map" element={<CourseMap />} />
+          <Route path="/lesson/:id" element={<LessonPlayer />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
