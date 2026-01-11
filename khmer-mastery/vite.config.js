5b0c0eb69ba595f595bfd6541c59bdb3688a546e
FIJY <fijy@mail.ru>
2026-01-11 10:58:58 +0300

    Fix: Downgrade build target to es2015 for mobile compatibility and add error logger

--- khmer-mastery/vite.config.js
+++ khmer-mastery/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    // ВАЖНО: Делаем код понятным для старых браузеров
+    target: 'es2015',
     outDir: 'dist',
     sourcemap: true
   },
